ff96a2b046779af5d7e078a502b5aea1369b6841
Rifat Hasan Priyo <137903375+rifathasanpriyo@users.noreply.github.com>
2024-07-28 00:21:03 +0600

    firebase set

--- flutter_chat_app/lib/main.dart
+++ flutter_chat_app/lib/main.dart
@@ -13,6 +13,7 @@ void main() async{
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: ChatPage(),
+        home: SignUpPage(),
       ),
     );
   }
--- flutter_chat_app/lib/signup_page.dart
+++ flutter_chat_app/lib/signup_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/chat_page.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -8,6 +10,36 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  String Name = "", Email = "", Password = "";
+
+  TextEditingController Namecontroller = new TextEditingController();
+  TextEditingController Emailcontroller = new TextEditingController();
+  TextEditingController Passwordcontroller = new TextEditingController();
+
+  final _fromkey = GlobalKey<FormState>();
+
+  SignUpFunction() async {
+  if (Password!=null && Namecontroller.text.isNotEmpty && Emailcontroller.text.isNotEmpty) {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: Email,
+        password: Password,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Sign Up Successful")));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "weak-password") {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Password is too weak")));
+      } else if (e.code == "email-already-in-use") {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("This email is already in use")));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("An error occurred")));
+    }
+  }
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,115 +91,153 @@ class _SignUpPageState extends State<SignUpPage> {
                           decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(10)),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Name",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        width: 1, color: Colors.black87),
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      prefixIcon: Icon(Icons.person_outline_outlined)),
-                                ),
-                              ),
-                              Text(
-                                "Email",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        width: 1, color: Colors.black87),
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      prefixIcon: Icon(Icons.mail_outline)),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 18,
-                              ),
-                              Text(
-                                "Password",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        width: 1, color: Colors.black87),
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      prefixIcon: Icon(Icons.key_outlined)),
-                                  obscureText: true,
-                                ),
-                              ),
-                         SizedBox(height: 10,),
+                          child: Form(
+                            key: _fromkey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
                                 Text(
-                                "Confirm Password",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        width: 1, color: Colors.black87),
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      prefixIcon: Icon(Icons.key_outlined)),
-                                  obscureText: true,
+                                  "Name",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w500),
                                 ),
-                              ),
-                              SizedBox(
-                                height: 30,
-                              ),
-                              Center(
-                                child: Material(
-                                  elevation: 15,
-                                  child: Container(
-                                    padding: EdgeInsets.all(10),
-                                    decoration: BoxDecoration(
-                                        color: Color(0xFF7f30fe),
-                                        borderRadius:
-                                            BorderRadius.circular(10)),
-                                    child: Text(
-                                      "Sign Up",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold),
-                                    ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 1, color: Colors.black87),
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: TextFormField(
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return "Please enter your name";
+                                      }
+                                      return null;
+                                    },
+                                    controller: Namecontroller,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        prefixIcon: Icon(
+                                            Icons.person_outline_outlined)),
                                   ),
                                 ),
-                              )
-                            ],
+                                Text(
+                                  "Email",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 1, color: Colors.black87),
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: TextFormField(
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return "Please enter your mail";
+                                      }
+                                      return null;
+                                    },
+                                    controller: Emailcontroller,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        prefixIcon: Icon(Icons.mail_outline)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 18,
+                                ),
+                                Text(
+                                  "Password",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 1, color: Colors.black87),
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: TextFormField(
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return "Please enter a password";
+                                      }
+                                      return null;
+                                    },
+                                    controller: Passwordcontroller,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        prefixIcon: Icon(Icons.key_outlined)),
+                                    obscureText: true,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "Confirm Password",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 1, color: Colors.black87),
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        prefixIcon: Icon(Icons.key_outlined)),
+                                    obscureText: true,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Center(
+                                  child: Material(
+                                    elevation: 15,
+                                    child: GestureDetector(
+                                      onTap: (){ 
+                                        if(_fromkey.currentState!.validate()){ 
+                                          setState(() {
+                                            Name=Namecontroller.text;
+                                            Email=Emailcontroller.text;
+                                            Password=Passwordcontroller.text;
+                                          });
+                                        }
+                                        SignUpFunction();
+                                      },
+                                      child: Container(
+                                        padding: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                            color: Color(0xFF7f30fe),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Text(
+                                          "Sign Up",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    
                   ],
                 ),
               )
